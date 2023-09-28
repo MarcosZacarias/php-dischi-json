ae71e7116a56742072d5d52bb1f72c25bf190d7a
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-09-28 17:15:32 +0200

    add: print cards with axios and file api

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,7 +3,20 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      message: "Hello World",
+      discs: [],
     };
+  },
+
+  mounted() {
+    // console.log(axios);
+    axios
+      .get(
+        "http://localhost/esercizio-46-php-dischi-json/php-dischi-json/backend/api/get-discs.php"
+      )
+      .then((response) => {
+        console.log(response.data);
+        this.discs = response.data;
+        console.log(this.discs);
+      });
   },
 }).mount("#app");
