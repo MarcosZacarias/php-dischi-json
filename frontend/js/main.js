44528101147ed045aad1384b5fa4c40c5af0b4ef
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-09-28 18:48:53 +0200

    add print card info

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -4,7 +4,31 @@ createApp({
   data() {
     return {
       discs: [],
+      cardInfo: {},
+      viewCardInfo: false,
     };
+  },
+
+  methods: {
+    printCardInfo(index) {
+      this.viewCardInfo = true;
+
+      console.log(index);
+
+      axios
+        .post(
+          "http://localhost/esercizio-46-php-dischi-json/php-dischi-json/backend/api/get-discs.php"
+        )
+        .then((response) => {
+          console.log(response.data);
+          this.cardInfo = response.data[index];
+          console.log(this.cardInfo);
+        });
+    },
+
+    closeCardInfo() {
+      this.viewCardInfo = false;
+    },
   },
 
   mounted() {
