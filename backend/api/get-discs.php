ff40f0dabebafd0e4683c6b7fbeada6fc1f3bbfd
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-09-28 17:49:39 +0200

    add det disp with file json

--- backend/api/get-discs.php
+++ backend/api/get-discs.php
@@ -1,5 +1,7 @@
 <?php
-include __DIR__ . "/../data/dischi.php";
+$json_discs = file_get_contents("../data/dischi_json.json");
+// var_dump($json_discs);
+$dischi = json_decode($json_discs);
 
 header("Content-Type: application/json");
 echo json_encode($dischi);
